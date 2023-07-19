d320521a349a9af5223d8433567e09a0392e2dcc
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-19 14:44:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['917798203711', 'SHUBHAM', true],
+  ['917821096044', 'BABY', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['917821096044'] 
+global.prems = ['917605902011', '917821096044', '917821096044']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -34,7 +34,10 @@ global.APIs = { // API Prefix
   akuari2: 'https://apimu.my.id',
   nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
+  fgmods: 'https://api-fgmods.ddns.net',
+   logo: 'https://oni-chan.my.id',
+  rose: 'https://api.itsrose.site',
+  zeltoria: 'https://api.zeltoria.my.id'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
@@ -47,17 +50,19 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝚂𝚢𝚗𝚝𝚊𝚡-𝚖𝚍'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.packname = 'syntax' 
+global.author = '@shubham' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com\n' 
+global.dygp = 'https://youtube.com/@samcreation8299'
+global.fgsc = 'https://GitHub.com' 
+global.fgyt = 'https://youtube.com/@samcreation8299'
+global.fgpyp = 'https://youtube.com/@samcreation8299'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-
+global.shizokeys = 'shizo'
+global.piesmsg = '_Pies 🥵💦_'
+global.rose = 'Rs-Danil_Elistz'
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
@@ -75,3 +80,4 @@ watchFile(file, () => {
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
+  
